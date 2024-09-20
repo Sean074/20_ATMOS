bf30324306423d7fa26a4a56ecc6537d718ad7b4
Sean O'Meara <sean_c_omeara@yahoo.com.au>
2024-09-20 14:39:24 -0700

    Feels like i did a lot of work but nothing gained.

--- atmos.py
+++ atmos.py
@@ -8,52 +8,70 @@ def select_atmos_data():
     return df_atmos
 
 
+def print_atmos(point_in_sky):
+    # TODO Let the user pick output units
+    print("\n======== ATMOS ==================================")
+    print("Point in the sky properties:")
+    print(f"Pressure Altitude [ft]: {point_in_sky['h_press_ft']}")
+    print(f"Pressure Static [psf]: {point_in_sky['p_static_psf']}")
+    print(f"Density Ratio: {point_in_sky['pho_ratio']}")
+    print(f"Air Density: [slug/ft^3]: {point_in_sky['pho_slug_ft3']}")
+    print("==================================================\n")
+
+
 def get_atmos_properties(h_press_ft,p_press_psf):
     df_atmos = select_atmos_data()
 
     # User Defined altitude h_press_ft
-    if h_press_ft != False:     
+    if p_press_psf == "NA":  
         # Test request is "on table"
         hgeo_min = df_atmos["Hgeo_ft"].min()
         hgeo_max = df_atmos["Hgeo_ft"].max()
 
         # Test h within table
         if hgeo_min >= h_press_ft:
-            print("ERROR: Altitude too low out of range")
-            exit()
+            print(f"ERROR: Altitude {h_press_ft} ft too low out of range {hgeo_min} to {hgeo_max} ft")
+            output_df = "ERROR"
         elif hgeo_max <= h_press_ft:
-            print("ERROR: Altitude too high out of range")
-            exit()
+            print(f"ERROR: Altitude {h_press_ft} ft too high out of range {hgeo_max} to {hgeo_max} ft")
+            output_df = "ERROR"
+        else:
+            p_press_psf  = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["P_lb/ft2"])
+            pho_ratio = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho/pho0"])
+            pho_slug_ft3 = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho_slug/ft3"])
+            
+            output_df = {
+                'h_press_ft':   h_press_ft,
+                'p_static_psf': p_press_psf,
+                'pho_ratio':    pho_ratio,
+                'pho_slug_ft3': pho_slug_ft3,
+            }
 
-        p_press_psf  = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["P_lb/ft3"])
-        pho_ratio = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho/pho0"])
-        pho_slug_ft3 = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho_slug/ft3"])
 
     else:
         # Test request is "on table"
-        press_min = df_atmos["P_lb/ft3"].min()
-        press_max = df_atmos["P_lb/ft3"].max()
+
+        press_min = df_atmos["P_lb/ft2"].min()
+        press_max = df_atmos["P_lb/ft2"].max()
 
         if press_min >= p_press_psf:
-            print("ERROR: Pressure too low out of range")
-            exit()
+            print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
+            output_df = "ERROR"
         elif press_max <= p_press_psf:
-            print("ERROR: Pressure too high out of range")
-            exit()      
-        
-        h_press_ft  = np.interp(p_press_psf, df_atmos["P_lb/ft3"], df_atmos["H_ft"])
-        pho_ratio = np.interp(p_press_psf, df_atmos["P_lb/ft3"], df_atmos["pho/pho0"])
-        pho_slug_ft3 = np.interp(p_press_psf, df_atmos["P_lb/ft3"], df_atmos["pho_slug/ft3"])
+            print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
+            output_df = "ERROR"      
+        else:
+            # TODO This interp function does not work UNLESS the x data is ordered in increasing.  Else crap.
+            h_press_ft  = np.interp(p_press_psf, df_atmos["P_lb/ft2"], df_atmos["H_ft"])
+            pho_ratio = np.interp(p_press_psf, df_atmos["P_lb/ft2"], df_atmos["pho/pho0"])
+            pho_slug_ft3 = np.interp(p_press_psf, df_atmos["P_lb/ft2"], df_atmos["pho_slug/ft3"])
 
-    output_df = {
-        'h_press_ft':   h_press_ft,
-        'p_static_psf': p_press_psf,
-        'pho_ratio':    pho_ratio,
-        'pho_slug_ft3': pho_slug_ft3,
-    }
+            output_df = {
+                'h_press_ft':   h_press_ft,
+                'p_static_psf': p_press_psf,
+                'pho_ratio':    pho_ratio,
+                'pho_slug_ft3': pho_slug_ft3,
+            }
 
     return output_df
-
-
-
 
--- menue.py
+++ menue.py
@@ -2,6 +2,46 @@ import unit_convert as u_c
 import atmos
 
 
+
+
+
+def pressure_convert(press_user,press_unit):
+
+    flag = False
+
+    while flag != True:
+        if press_unit.lower() == "psi":
+            press_psf = press_user * u_c.PSI_PSF
+            flag = True
+        elif press_unit.lower() == "psf":
+            press_psf = press_user * 1.0
+            flag = True
+        elif press_unit.lower() == "pa":
+            press_psf = press_user * u_c.PA_PSF
+            flag = True
+        elif press_unit.lower() == "atm":
+            press_psf = press_user * u_c.ATM_PSF
+            flag = True
+        elif press_unit.lower() == "bar":
+            press_psf = press_user * u_c.BAR_PSF
+            flag = True
+        elif press_unit.lower() == "m":
+            h_press_ft = press_user * u_c.M_FT
+            point_in_sky = atmos.get_atmos_properties(h_press_ft=h_press_ft,p_press_psf="NA")
+            press_psf = point_in_sky['p_static_psf']
+            flag = True
+        elif press_unit.lower() == "ft":
+            h_press_ft = press_user
+            point_in_sky = atmos.get_atmos_properties(h_press_ft=h_press_ft,p_press_psf="NA")
+            press_psf = point_in_sky['p_static_psf']
+            flag = True
+        else:
+            print("Input error: Select pfs/psi/pa/atm/bar")
+            press_unit = input("Input units pfs/psi/pa/atm/bar: ")
+
+    return (press_psf)
+
+
 def air_data_alt():
     h_press_user = float(input("Input pressure altitude: "))
     h_unit = input("Input units m/ft: ")
@@ -19,46 +59,42 @@ def air_data_alt():
             print("Input error: Select m or ft")
             h_unit = input("Input units m/ft: ")
 
-    point_in_sky = atmos.get_atmos_properties(h_press_ft=h_press_ft,p_press_psf=False)
+    point_in_sky = atmos.get_atmos_properties(h_press_ft=h_press_ft, p_press_psf="NA")
 
-    # TODO Make this look pretty
-    # TODO Let the user pick output units
-    print(point_in_sky)
+    if point_in_sky == "ERROR":
+        pass
+    else:
+        atmos.print_atmos(point_in_sky)
 
     return
 
+
 def air_data_pres():
     press_user = float(input("Input pressure: "))
-    press_unit = input("Input units pfs/psi/pa/atm/bar: ")
+    press_unit = input("Input units psf/psi/pa/atm/bar: ")
 
-    flag = False
+    press_psf = pressure_convert(press_user,press_unit)
 
-    while flag != True:
-        if press_unit.lower() == "psi":
-            press_psi = press_user * u_c.PSI_PSF
-            flag = True
-        elif press_unit.lower() == "pa":
-            press_psi = press_user * u_c.PA_PSF
-            flag = True
-        elif press_unit.lower() == "atm":
-            press_psi = press_user * u_c.ATM_PSF
-            flag = True
-        elif press_unit.lower() == "bar":
-            press_psi = press_user * u_c.BAR_PSF
-            flag = True
-        else:
-            print("Input error: Select pfs/psi/pa/atm/bar")
-            press_unit = input("Input units pfs/psi/pa/atm/bar: ")
+    print(f"Pressure {press_psf}")
 
-    point_in_sky = atmos.get_atmos_properties(h_press_ft=False,p_press_psf=press_psi)
+    point_in_sky = atmos.get_atmos_properties(h_press_ft="NA",p_press_psf=press_psf)
 
-    # TODO Make this look pretty
-    print(point_in_sky)
+    if point_in_sky == "ERROR":
+        pass
+    else:
+        atmos.print_atmos(point_in_sky)
 
     return
 
 def alt_mach():
-    pass
+    # TODO Make this valid supersonic
+
+    # Request user input
+    alt = input("Input the pressure altitude: ")
+    alt_units = input("Input the pressure altitude units: ")
+    mach  = input() 
+
+    return
 
 
 def alt_true():
