e044c5736349cd3671142791bf9bb3a6a46f8741
Sean O'Meara <sean_c_omeara@yahoo.com.au>
2024-09-22 16:13:52 -0500

    Subsonic airspeed calculation is implimneted

--- atmos.py
+++ atmos.py
@@ -20,73 +20,237 @@ def print_atmos(point_in_sky):
     print("==================================================\n")
 
 
-def get_atmos_properties(h_press_ft,p_press_psf):
+def get_atmos_prop_alt(h_press_ft):
     df_atmos = select_atmos_data()
 
-    # User Defined altitude h_press_ft
-    if p_press_psf == "NA":  
-        # Test request is "on table"
-        hgeo_min = df_atmos["Hgeo_ft"].min()
-        hgeo_max = df_atmos["Hgeo_ft"].max()
+    # Test request is "on table"
+    hgeo_min = df_atmos["Hgeo_ft"].min()
+    hgeo_max = df_atmos["Hgeo_ft"].max()
 
-        # Test h within table
-        if hgeo_min >= h_press_ft:
-            print(f"ERROR: Altitude {h_press_ft} ft too low out of range {hgeo_min} to {hgeo_max} ft")
-            output_df = "ERROR"
-        elif hgeo_max <= h_press_ft:
-            print(f"ERROR: Altitude {h_press_ft} ft too high out of range {hgeo_max} to {hgeo_max} ft")
-            output_df = "ERROR"
-        else:
-            p_press_psf  = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["P_lb/ft2"])
-            pho_ratio = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho/pho0"])
-            pho_slug_ft3 = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho_slug/ft3"])
-            
-            output_df = {
-                'h_press_ft':   h_press_ft,
-                'p_static_psf': p_press_psf,
-                'pho_ratio':    pho_ratio,
-                'pho_slug_ft3': pho_slug_ft3,
-            }
+    print(f"hgeo_min {hgeo_min} and h_press_ft {h_press_ft}")
 
-
+    # Test h within table
+    if hgeo_min >= h_press_ft:
+        print(f"ERROR: Altitude {h_press_ft} ft too low out of range {hgeo_min} to {hgeo_max} ft")
+        output_df = "ERROR"
+    elif hgeo_max <= h_press_ft:
+        print(f"ERROR: Altitude {h_press_ft} ft too high out of range {hgeo_max} to {hgeo_max} ft")
+        output_df = "ERROR"
     else:
-        # Test request is "on table"
+        p_press_psf  = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["P_lb/ft2"])
+        pho_ratio = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho/pho0"])
+        pho_slug_ft3 = np.interp(h_press_ft, df_atmos["Hgeo_ft"], df_atmos["pho_slug/ft3"])
+        
+        output_df = {
+            'h_press_ft':   h_press_ft,
+            'p_static_psf': p_press_psf,
+            'pho_ratio':    pho_ratio,
+            'pho_slug_ft3': pho_slug_ft3,
+        }
+    return output_df
 
-        press_min = df_atmos["P_lb/ft2"].min()
-        press_max = df_atmos["P_lb/ft2"].max()
 
-        if press_min >= p_press_psf:
-            print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
-            output_df = "ERROR"
-        elif press_max <= p_press_psf:
-            print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
-            output_df = "ERROR"      
-        else:
-            # Interp requires the x value ot be asending.
-            df_atmos_sort_p = df_atmos.sort_values(by=["P_lb/ft2"])
-            h_press_ft  = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["H_ft"])
-            pho_ratio = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["pho/pho0"])
-            pho_slug_ft3 = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["pho_slug/ft3"])
+def get_atmos_prop_pres(p_press_psf):
+    df_atmos = select_atmos_data()
+    # Test request is "on table"
 
-            output_df = {
-                'h_press_ft':   h_press_ft,
-                'p_static_psf': p_press_psf,
-                'pho_ratio':    pho_ratio,
-                'pho_slug_ft3': pho_slug_ft3,
-            }
+    press_min = df_atmos["P_lb/ft2"].min()
+    press_max = df_atmos["P_lb/ft2"].max()
+
+    if press_min >= p_press_psf:
+        print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
+        output_df = "ERROR"
+    elif press_max <= p_press_psf:
+        print(f"ERROR: Altitude {p_press_psf} psf too low out of range {press_min} to {press_max} psf")
+        output_df = "ERROR"      
+    else:
+        # Interp requires the x value ot be asending.
+        df_atmos_sort_p = df_atmos.sort_values(by=["P_lb/ft2"])
+        h_press_ft  = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["H_ft"])
+        pho_ratio = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["pho/pho0"])
+        pho_slug_ft3 = np.interp(p_press_psf, df_atmos_sort_p["P_lb/ft2"], df_atmos_sort_p["pho_slug/ft3"])
+
+        output_df = {
+            'h_press_ft':   h_press_ft,
+            'p_static_psf': p_press_psf,
+            'pho_ratio':    pho_ratio,
+            'pho_slug_ft3': pho_slug_ft3,
+        }
 
     return output_df
 
 
-def mach_alt(speed_defined,alt_defined):
+def mach_alt(speed,alt_defined):
+    # TODO convert a_0 to calcualted from user supplied atmos data
+    # TODO make it good for supersonic flight
+
+    df_atmos = select_atmos_data()
+
+    # Constants
+    a_0 = 661.47  # sealevel speed of sound kts
+    p_0 = df_atmos.loc[df_atmos["Hgeo_ft"]==0, "P_lb/ft2"].values[0] 
+    gamma = 1.4     # ratio of specific heats for air
+
+    # Get the atmos properties for the user selected altitude.
+    atmos_alt = get_atmos_prop_alt(alt_defined)
+    p_static = atmos_alt['p_static_psf']
+    pho = atmos_alt['pho_slug_ft3']
+    pho_ratio = atmos_alt['pho_ratio']
+    a_local = math.sqrt(gamma*p_static/pho)*0.5924838  # kts
     
-    a_0        = 661.47  # kts
-    gamma      = 1.4     # ratio of specific heats for air
+    # Calculate speed Sub Sonic
+    Mach = speed
+    ktas = Mach * a_local
+    keas = ktas * math.sqrt(pho_ratio)
+    q_ = p_static * ((1 + 0.2 * Mach**2)**(7/2)-1)
+    kcas = a_0 * math.sqrt(5*((q_/p_0+1)**(2/7)-1))
 
-    aTarget    = math.sqrt(gamma*atmos['p_static_psf']/atmos['pho_slug_ft3'])*0.5924838  # kts, p [], pho []
+    speed_df = {
+        "Mach": Mach,
+        "ktas": ktas,
+        "keas": keas,
+        "q": q_,
+        "kcas": kcas,
+    }
 
-    MachTarget = speed_defined
-    KTASTarget = aTarget*MachTarget
-    KEASTarget = KTASTarget*math.sqrt(atmos['pho_ratio'])
-    qTarget    = atmos['p_static_psf']*((1+0.2*MachTarget**2)**(7/2)-1)
-    KCASTarget = a_0*math.sqrt(5*((qTarget/p_0+1)**(2/7)-1))
+    return speed_df
+
+def tas_alt(speed,alt_defined):
+    # TODO convert a_0 to calcualted from user supplied atmos data
+    # TODO make it good for supersonic flight
+    
+    df_atmos = select_atmos_data()
+
+    # Constants
+    a_0 = 661.47  # kts
+    p_0 = df_atmos.loc[df_atmos["Hgeo_ft"]==0, "P_lb/ft2"].values[0] 
+    gamma = 1.4     # ratio of specific heats for air
+
+    # Get the atmos properties for the user selected altitude.
+    atmos_alt = get_atmos_prop_alt(alt_defined)
+    p_static = atmos_alt['p_static_psf']
+    pho = atmos_alt['pho_slug_ft3']
+    pho_ratio = atmos_alt['pho_ratio']
+    a_local = math.sqrt(gamma*p_static/pho)*0.5924838  # kts
+    
+    # Calculate speed Sub Sonic
+    ktas = speed
+    Mach = ktas / a_local
+    keas = ktas * math.sqrt(pho_ratio)
+    q_ = p_static * ((1 + 0.2 * Mach**2)**(7/2)-1)
+    kcas = a_0 * math.sqrt(5*((q_/p_0+1)**(2/7)-1))
+
+    speed_df = {
+        "Mach": Mach,
+        "ktas": ktas,
+        "keas": keas,
+        "q": q_,
+        "kcas": kcas,
+    }
+
+    return speed_df
+
+def eas_alt(speed,alt_defined):
+    # TODO convert a_0 to calcualted from user supplied atmos data
+    # TODO make it good for supersonic flight
+  
+    df_atmos = select_atmos_data()
+
+    # Constants
+    a_0 = 661.47  # kts
+    p_0 = df_atmos.loc[df_atmos["Hgeo_ft"]==0, "P_lb/ft2"].values[0] 
+    gamma = 1.4     # ratio of specific heats for air
+
+    # Get the atmos properties for the user selected altitude.
+    atmos_alt = get_atmos_prop_alt(alt_defined)
+    p_static = atmos_alt['p_static_psf']
+    pho = atmos_alt['pho_slug_ft3']
+    pho_ratio = atmos_alt['pho_ratio']
+    a_local = math.sqrt(gamma*p_static/pho)*0.5924838  # kts
+    
+    # Calculate speed Sub Sonic
+    keas = speed
+    ktas = keas / math.sqrt(pho_ratio)
+    Mach = ktas / a_local
+    q_ = p_static * ((1 + 0.2 * Mach**2)**(7/2)-1)
+    kcas = a_0 * math.sqrt(5*((q_/p_0+1)**(2/7)-1))
+
+    speed_df = {
+        "Mach": Mach,
+        "ktas": ktas,
+        "keas": keas,
+        "q": q_,
+        "kcas": kcas,
+    }
+
+    return speed_df
+
+def eas_alt(speed,alt_defined):
+    # TODO convert a_0 to calcualted from user supplied atmos data
+    # TODO make it good for supersonic flight
+   
+    df_atmos = select_atmos_data()
+
+    # Constants
+    a_0 = 661.47  # kts
+    p_0 = df_atmos.loc[df_atmos["Hgeo_ft"]==0, "P_lb/ft2"].values[0] 
+    gamma = 1.4     # ratio of specific heats for air
+
+    # Get the atmos properties for the user selected altitude.
+    atmos_alt = get_atmos_prop_alt(alt_defined)
+    p_static = atmos_alt['p_static_psf']
+    pho = atmos_alt['pho_slug_ft3']
+    pho_ratio = atmos_alt['pho_ratio']
+    a_local = math.sqrt(gamma*p_static/pho)*0.5924838  # kts
+    
+    # Calculate speed Sub Sonic
+    keas = speed
+    ktas = keas / math.sqrt(pho_ratio)
+    Mach = ktas / a_local
+    q_ = p_static * ((1 + 0.2 * Mach**2)**(7/2)-1)
+    kcas = a_0 * math.sqrt(5*((q_/p_0+1)**(2/7)-1))
+
+    speed_df = {
+        "Mach": Mach,
+        "ktas": ktas,
+        "keas": keas,
+        "q": q_,
+        "kcas": kcas,
+    }
+
+    return speed_df
+
+def cas_alt(speed,alt_defined):
+    # TODO convert a_0 to calcualted from user supplied atmos data
+    # TODO make it good for supersonic flight
+     
+    df_atmos = select_atmos_data()
+
+    # Constants
+    a_0 = 661.47  # kts
+    p_0 = df_atmos.loc[df_atmos["Hgeo_ft"]==0, "P_lb/ft2"].values[0] 
+    gamma = 1.4     # ratio of specific heats for air
+
+    # Get the atmos properties for the user selected altitude.
+    atmos_alt = get_atmos_prop_alt(alt_defined)
+    p_static = atmos_alt['p_static_psf']
+    pho = atmos_alt['pho_slug_ft3']
+    pho_ratio = atmos_alt['pho_ratio']
+    a_local = math.sqrt(gamma*p_static/pho)*0.5924838  # kts
+    
+    # Calculate speed Sub Sonic
+    kcas = speed
+    q_ =  p_0*((0.2*(kcas/a_0)**2+1)**(7/2)-1)
+    Mach = math.sqrt(5*((q_/p_static+1)**(2/7)-1))
+    ktas = Mach * a_local
+    keas = ktas * math.sqrt(pho_ratio)
+
+    speed_df = {
+        "Mach": Mach,
+        "ktas": ktas,
+        "keas": keas,
+        "q": q_,
+        "kcas": kcas,
+    }
+
+    return speed_df
--- menue.py
+++ menue.py
@@ -2,9 +2,6 @@ import unit_convert as u_c
 import atmos
 
 
-
-
-
 def pressure_convert(press_user,press_unit):
 
     flag = False
@@ -59,7 +56,7 @@ def air_data_alt():
             print("Input error: Select m or ft")
             h_unit = input("Input units m/ft: ")
 
-    point_in_sky = atmos.get_atmos_properties(h_press_ft=h_press_ft, p_press_psf="NA")
+    point_in_sky = atmos.get_atmos_prop_alt(h_press_ft=h_press_ft)
 
     if point_in_sky == "ERROR":
         pass
@@ -77,7 +74,7 @@ def air_data_pres():
 
     print(f"Pressure {press_psf}")
 
-    point_in_sky = atmos.get_atmos_properties(h_press_ft="NA",p_press_psf=press_psf)
+    point_in_sky = atmos.get_atmos_prop_pres(p_press_psf=press_psf)
 
     if point_in_sky == "ERROR":
         pass
@@ -86,36 +83,151 @@ def air_data_pres():
 
     return
 
+
 def alt_mach():
     # TODO Make this valid supersonic
 
     # Request user input
-    alt_user = input("Input the pressure altitude: ")
+    alt_user = float(input("Input the pressure altitude: "))
     alt_unit = input("Input the pressure altitude units (ft/m): ")
-    mach  = input()
+    mach  = float(input("Input the Mach: "))
+    
     flag = False
 
     while flag != True:
         if alt_unit.lower() == "m":
             h_press_ft = alt_user * u_c.M_FT
             flag = True
-        elif h_unit.lower() == "ft":
+        elif alt_unit.lower() == "ft":
             h_press_ft = alt_user
             flag = True
         else:
             print("Input error: Select m or ft")
-            h_unit = input("Input units m/ft: ")
+            alt_unit = input("Input units m/ft: ")
 
+    speeds = atmos.mach_alt(mach,h_press_ft)
+    print(speeds)
 
     return
 
 
 def alt_true():
-    pass
+    # TODO Make this valid supersonic
+
+    # Request user input
+    alt_user = float(input("Input the pressure altitude: "))
+    alt_unit = input("Input the pressure altitude units (ft, m): ")
+    tas  = float(input("Input the True Air Speed TAS: "))
+    tas_unit = input("Input speed units (kts, m/s): ")
+
+    # Convert altitude to base units
+    flag = False
+    while flag != True:
+        if alt_unit.lower() == "m":
+            h_press_ft = alt_user * u_c.M_FT
+            flag = True
+        elif alt_unit.lower() == "ft":
+            h_press_ft = alt_user
+            flag = True
+        else:
+            print("Input error: Select m or ft")
+            alt_unit = input("Input units (m, ft): ")
+
+    # Convert speed to kts
+    flag = False
+    while flag != True:
+        if tas_unit.lower() == "m/s":
+            ktas = tas * u_c.MS_KTS
+            flag = True
+        elif tas_unit.lower() == "kts":
+            ktas = tas
+            flag = True
+        else:
+            print("Input error: Select m/s or kts")
+            alt_unit = input("Input units (m/s, kts): ")
+
+    speeds = atmos.tas_alt(ktas,h_press_ft)
+    print(speeds)
+
+    return
 
 def alt_cal():
-    pass
+    # TODO Make this valid supersonic
+
+    # Request user input
+    alt_user = float(input("Input the pressure altitude: "))
+    alt_unit = input("Input the pressure altitude units (ft, m): ")
+    cas  = float(input("Input the Calibrated Air Speed CAS: "))
+    cas_unit = input("Input speed units (kts, m/s): ")
+
+    # Convert altitude to base units
+    flag = False
+    while flag != True:
+        if alt_unit.lower() == "m":
+            h_press_ft = alt_user * u_c.M_FT
+            flag = True
+        elif alt_unit.lower() == "ft":
+            h_press_ft = alt_user
+            flag = True
+        else:
+            print("Input error: Select m or ft")
+            alt_unit = input("Input units (m, ft): ")
+
+    # Convert speed to kts
+    flag = False
+    while flag != True:
+        if cas_unit.lower() == "m/s":
+            kcas = cas * u_c.MS_KTS
+            flag = True
+        elif cas_unit.lower() == "kts":
+            kcas = cas
+            flag = True
+        else:
+            print("Input error: Select m/s or kts")
+            alt_unit = input("Input units (m/s, kts): ")
+
+    speeds = atmos.cas_alt(kcas,h_press_ft)
+    print(speeds)
+
+    return
 
 
 def alt_equiv():
-    pass
+    # TODO Make this valid supersonic
+
+    # Request user input
+    alt_user = float(input("Input the pressure altitude: "))
+    alt_unit = input("Input the pressure altitude units (ft, m): ")
+    eas  = float(input("Input the Equiv Air Speed EAS: "))
+    eas_unit = input("Input speed units (kts, m/s): ")
+
+    # Convert altitude to base units
+    flag = False
+    while flag != True:
+        if alt_unit.lower() == "m":
+            h_press_ft = alt_user * u_c.M_FT
+            flag = True
+        elif alt_unit.lower() == "ft":
+            h_press_ft = alt_user
+            flag = True
+        else:
+            print("Input error: Select m or ft")
+            alt_unit = input("Input units (m, ft): ")
+
+    # Convert speed to kts
+    flag = False
+    while flag != True:
+        if eas_unit.lower() == "m/s":
+            keas = eas * u_c.MS_KTS
+            flag = True
+        elif eas_unit.lower() == "kts":
+            keas = eas
+            flag = True
+        else:
+            print("Input error: Select m/s or kts")
+            alt_unit = input("Input units (m/s, kts): ")
+
+    speeds = atmos.eas_alt(keas,h_press_ft)
+    print(speeds)
+
+    return
